11ff9a55c75c663af002237f9ce59a79b2918269
Hassan Ali <hassan.ali@empired.com>
2019-12-12 12:21:29 +1100

    re-write using Maps

--- src/day-3/__tests__/main.test.ts
+++ src/day-3/__tests__/main.test.ts
@@ -11,82 +11,41 @@ describe('Day 3: Part 1', () => {
     expect(convertPath('R1')).toMatchObject(['R', 1]);
   });
 
-  test('Convert a path to a hashmap', () => {
+  test('Convert a path to a map', () => {
     const path = 'R1,U1';
-    expect(mapPath(path)).toMatchObject({ '1_0': '-', '1_1': '-' });
-  });
-
-  test('Convert a longer path to a hashmap', () => {
-    const path = 'R8,U5,L5,D3';
-    expect(mapPath(path)).toMatchObject({
-      '1_0': '-',
-      '2_0': '-',
-      '3_0': '-',
-      '4_0': '-',
-      '5_0': '-',
-      '6_0': '-',
-      '7_0': '-',
-      '8_0': '-',
-      '8_1': '-',
-      '8_2': '-',
-      '8_3': '-',
-      '8_4': '-',
-      '8_5': '-',
-      '7_5': '-',
-      '6_5': '-',
-      '5_5': '-',
-      '4_5': '-',
-      '3_5': '-',
-      '3_4': '-',
-      '3_3': '-',
-      '3_2': '-'
-    });
-  });
-
-  test('Map two paths onto one hashmaps', () => {
-    const path1 = 'R1,U1';
-    const path2 = 'U1,R1';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-
-    expect(mergedPaths).toMatchObject({ '1_0': 'A', '1_1': 'AB' });
-    expect(mergedPaths).toMatchObject({ '0_1': 'B', '1_1': 'AB' });
-  });
-
-  test('Mark intersects of two paths on one hashmaps', () => {
-    const path1 = 'R1,U1';
-    const path2 = 'U1,R1';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    expect(mergedPaths).toMatchObject({ '1_0': 'A', '1_1': 'AB' });
-    expect(mergedPaths).toMatchObject({ '0_1': 'B', '1_1': 'AB' });
-    expect(mergedPaths).toMatchObject({ '1_1': 'AB' });
+    const answer = new Map([
+      ['1,0', 1],
+      ['1,1', 2]
+    ]);
+    expect(mapPath(path)).toEqual(answer);
   });
 
   test('Get the intersections of two paths', () => {
     const path1 = 'R1,U1';
     const path2 = 'U1,R1';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    const intersections = getIntersections(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
 
-    expect(intersections).toEqual([[1, 1]]);
+    expect(intersections).toEqual(['1,1']);
   });
 
   test('Get the intersections of sample: R8,U5,L5,D3 & U7,R6,D4,L4', () => {
     const path1 = 'R8,U5,L5,D3';
     const path2 = 'U7,R6,D4,L4';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    const intersections = getIntersections(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
 
-    expect(intersections).toEqual([
-      [6, 5],
-      [3, 3]
-    ]);
+    expect(intersections).toEqual(['6,5', '3,3']);
   });
 
   test('Calculate Manhattan distance of sample: R8,U5,L5,D3 & U7,R6,D4,L4', () => {
     const path1 = 'R8,U5,L5,D3';
     const path2 = 'U7,R6,D4,L4';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    const intersections = getIntersections(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
     const distance = getManhattanDistance(intersections);
 
     expect(distance).toBe(6);
@@ -95,8 +54,9 @@ describe('Day 3: Part 1', () => {
   test('Calculate Manhattan distance of sample: R75,D30,R83,U83,L12,D49,R71,U7,L72 & U62,R66,U55,R34,D71,R55,D58,R83', () => {
     const path1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
     const path2 = 'U62,R66,U55,R34,D71,R55,D58,R83';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    const intersections = getIntersections(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
     const distance = getManhattanDistance(intersections);
 
     expect(distance).toBe(159);
@@ -105,10 +65,10 @@ describe('Day 3: Part 1', () => {
   test('Calculate Manhattan distance of sample: R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 & U98,R91,D20,R16,D67,R40,U7,R15,U6,R7', () => {
     const path1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51';
     const path2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7';
-    const mergedPaths = mapPath(path2, mapPath(path1, {}, 'A'), 'B');
-    const intersections = getIntersections(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
     const distance = getManhattanDistance(intersections);
-
     expect(distance).toBe(135);
   });
 });
@@ -117,40 +77,32 @@ describe('Day 3: Part 2', () => {
   test('Get the number of steps to two paths intersections', () => {
     const path1 = 'R8,U5,L5,D3';
     const path2 = 'U7,R6,D4,L4';
-    const mergedPaths = mapPath(
-      path2,
-      mapPath(path1, {}, 'A', true),
-      'B',
-      true
-    );
-    const smallest = getClosestIntersection(mergedPaths);
-    expect(smallest).toBe(30);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
+    const closest = getClosestIntersection(map1, map2, intersections);
+
+    expect(closest).toBe(30);
   });
 
   test('R75,D30,R83,U83,L12,D49,R71,U7,L72 & U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps', () => {
     const path1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
     const path2 = 'U62,R66,U55,R34,D71,R55,D58,R83';
-    const mergedPaths = mapPath(
-      path2,
-      mapPath(path1, {}, 'A', true),
-      'B',
-      true
-    );
-    const smallest = getClosestIntersection(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
+    const closest = getClosestIntersection(map1, map2, intersections);
 
-    expect(smallest).toBe(610);
+    expect(closest).toBe(610);
   });
 
   test('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 & U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps', () => {
     const path1 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51';
     const path2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7';
-    const mergedPaths = mapPath(
-      path2,
-      mapPath(path1, {}, 'A', true),
-      'B',
-      true
-    );
-    const closest = getClosestIntersection(mergedPaths);
+    const map1 = mapPath(path1);
+    const map2 = mapPath(path2);
+    const intersections = getIntersections(map1, map2);
+    const closest = getClosestIntersection(map1, map2, intersections);
 
     expect(closest).toBe(410);
   });
